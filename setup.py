9119c7dd2ef7cd0fb93c078685f2a5ab05c6b426
Francesco Filicetti <francesco.filicetti@unical.it>
2019-09-06 09:45:37 +0200

    fix manifest

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-form-builder',
-    version='0.1-beta',
+    version='0.1',
     packages=find_packages(),
     include_package_data=True,
     license='BSD License',
