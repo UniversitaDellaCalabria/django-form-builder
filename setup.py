2235115b4270065b17f082677948e2ef091bf585
Francesco Filicetti <francesco.filicetti@unical.it>
2020-05-15 13:19:43 +0200

    setup.py v0.9.16

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-form-builder',
-    version='0.9.14',
+    version='0.9.16',
     packages=find_packages(),
     package_data={'': ['*.wav']},
     include_package_data=True,
